acbbda928cc36c43eb5400b9768fe79cd6fc1f3c
ImpDev <leoe25939@gmail.com>
2024-04-30 20:56:04 -0600

    Send JS objects available

--- websocket-cliente/script.js
+++ websocket-cliente/script.js
@@ -10,14 +10,30 @@ ws.addEventListener('open', (evt) => {
 
 ws.addEventListener('message',(event) => {
     //Paramatro "event" que sera aquella informacion que me dara el servidor
-    console.log(`Recibiendo del servidor: ${event.data}`)
+    //?Como podemos recibir ese JSON como JSON y no como String para manipularlo
+    //*Pues convirtiendolo a un objeto JSON
+    console.log("Recibiendo del servidor: ")
+    console.log(JSON.parse(event.data)) //Convertimos el objeto mandado como String por parte del usuario a JSON
 })
 
 const btnSend = document.getElementById("btnSend")
 //Mandar informacion al servidor
 btnSend.addEventListener('click',(event) => {
     event.preventDefault()
-    ws.send(inputMessage.value)
+
+    //!Ahora deseamos enviar objetos, como se realiza pues no podemos enviar objetos javaScript
+    //En realidad si se puede pero lo enviara de una manera ilegible
+
+    //Creacion del objeto JS
+    const person = {
+        name: "David",
+        lastname:"Ateno",
+        age:18,
+        isOlder:true
+    }
+
+    //Solo convertiremos este objeto JS en un String
+    ws.send(JSON.stringify(person)) //De esta manera podemos enviar JSON
 })
 
 btnClose.addEventListener('click',(event) => {
